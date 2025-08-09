ec7b5f1562123fc7d6b8905c978335ea4fdfe685
Abhay Singh <132543962+abhay-singh1100@users.noreply.github.com>
2025-08-09 10:44:54 +0530

    updated the logger file

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
+import logging
 import sys 
 
 def error_message_detail(error, error_detail:sys):
-    ## will ggive all the inforamntion on which line and file the error and excceoption has occured
     
     _,_,exc_tb= error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename 
@@ -14,8 +14,9 @@ def error_message_detail(error, error_detail:sys):
     
 class CustonException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+    
