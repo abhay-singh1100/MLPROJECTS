04397376c81dbaf333fdb3cb483a4e21a80902f6
Abhay Singh <132543962+abhay-singh1100@users.noreply.github.com>
2025-08-10 10:04:55 +0530

    modeltraining

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
-import logging
+
 import sys 
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     
@@ -20,3 +21,10 @@ class CustonException(Exception):
     def __str__(self):
         return self.error_message
     
+
+if __name__ == "__main__":
+    try :
+        a=1/0
+    except Exception as e:
+        logging.error("ZERO DIVIDED ERROR")
+        raise CustonException(e, sys)
