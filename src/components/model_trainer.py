562b107fed2c85a4862d506e05ea3f5ae76e48c0
Abhay Singh <132543962+abhay-singh1100@users.noreply.github.com>
2025-08-16 13:01:55 +0530

    completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,7 +24,7 @@ from src.utils import save_object, evaluate_models
 @dataclass
 
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts0","model.pkl")
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
 
 
 class ModelTrainer:
